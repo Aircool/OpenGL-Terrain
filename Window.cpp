0d829d4b9ec9b596c81d0b2da82b2a1f0c0c8eef
Caleb Meyer <calebzmeyer@gmail.com>
2016-11-10 15:02:55 -0800

    Removed Multi-Threading
    Made Mesh Width and Height defined intead of just square in terms of
    number of vertices. Removed Multi-threading because if the GL_Context is
    defined in the main thread no image is rendered. Might re-introduce
    Multi-threading to separate the user input from the rendering thread. In
    order to do this, I would have to separate my initSDL() function into
    two parts where OpenGL specific code (since the SDL portion is needed in
    the main thread for user input) is called in the render thread.

--- Window.cpp
+++ Window.cpp
@@ -4,42 +4,53 @@
 #include "TriangleMesh.h"
 
 SDL_Window* window;
+SDL_GLContext context;
+
 bool quit = false;
+TriangleMesh* TMesh;
 
-int render(void* ptr){
+int initSDL(){
+
+	if(SDL_Init(SDL_INIT_VIDEO) < 0)	return -1;
+
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetSwapInterval(1);
 	
-	SDL_GLContext context = SDL_GL_CreateContext(window);
-	
+	window = SDL_CreateWindow("OpenGL 3D", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+	if(!window)	return -1;
+
+	context = SDL_GL_CreateContext(window);
+
 	glewExperimental = GL_TRUE;
-	if(glewInit() != GLEW_OK){
-		
-		return -1;
-	}
-	
-	TriangleMesh TMesh(50);
+	if(glewInit() != GLEW_OK)	return -1;
 
-	while(!quit){
-		
-		glClearColor(0.0, 0.0, 0.0, 1.0);
-		glClear(GL_COLOR_BUFFER_BIT);
-		
-		TMesh.draw();
-		
-		SDL_GL_SwapWindow(window);
-		SDL_Delay(1000 / 60);
-	}
-	
-	SDL_GL_DeleteContext(context);
-	
 	return 0;
+}
+
+int initResources(){
+
+	TMesh = new TriangleMesh(160, 90);
+	return 0;
+}
+
+void render(){
+	
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	
+	TMesh->draw();
+
+	SDL_GL_SwapWindow(window);
 }
 
 void input(){
 	
 	SDL_Event event;
-	while(!quit){
-		
-		SDL_WaitEvent(&event);
+	if(SDL_PollEvent(&event)){
 		
 		if(event.type == SDL_QUIT) quit = true;
 		
@@ -49,30 +60,24 @@ void input(){
 	}
 }
 
+void loop(){
+
+	while(!quit){
+
+		render();
+		input();
+		SDL_Delay(1000/ 60);
+	}
+}
+
 int main(int argc, char* argv[]){
 	
-	if(SDL_Init(SDL_INIT_VIDEO) < 0){
-		
-		return -1;
-	}
-	
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-	SDL_GL_SetSwapInterval(1);
-	
-	window = SDL_CreateWindow("OpenGL 3D", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
-	if(!window){
-		
-		return -1;
-	}
-	
-	SDL_Thread* renderThread = SDL_CreateThread(render, "Render Thread", (void *) NULL);
-	SDL_DetachThread(renderThread);
-	
-	input();
-	
+	if(initSDL() < 0) return -1;
+	if(initResources() < 0) return -1;
+
+	loop();
+
+	SDL_GL_DeleteContext(context);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 	
